9393fc6270463b26823a08e566d6e212dbe4a1b9
Alex <amarcoli@ucsc.edu>
2014-07-15 20:56:49 -0700

    Updates to Tilemap and Vector2int

---
+++ Assets/Scripts/Slime/Slime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slime : MonoBehaviour {
+    private Tile _myTile;
+    private int _emptyNeighbors = 0;
+
+    public void Awake() {
+        _myTile = GetComponent<Tile>();
+    }
+
+    private void updateNeighborCount() {
+        Tilemap tilemap = null;
+        
+    }
+}

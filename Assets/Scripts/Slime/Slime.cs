49559085dc9618c6dbb5cf93ba9991171c1e2285
Alex <amarcoli@ucsc.edu>
2014-07-16 20:07:56 -0700

    Added neighbor count to slime

--- Assets/Scripts/Slime/Slime.cs
+++ Assets/Scripts/Slime/Slime.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Slime : MonoBehaviour {
     private Tile _myTile;
-    private int _emptyNeighbors = 0;
+    private int _slimeNeighbors = 0;
+
+    private Tilemap _tilemap;
 
     public void Awake() {
+        _tilemap = Tilemap.getInstance();
         _myTile = GetComponent<Tile>();
     }
 
     private void updateNeighborCount() {
-        Tilemap tilemap = null;
-        
+        List<Tile> neighbors = _tilemap.getNeighboringTiles(transform.position);
+        _slimeNeighbors = 0;
+        foreach (Tile tile in neighbors) {
+            if (tile.GetComponent<Slime>() != null) {
+                _slimeNeighbors++;
+            }
+        }
     }
 }

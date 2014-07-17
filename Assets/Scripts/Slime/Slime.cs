e26a22bb90f78608966a82cb6073a7db3fdbcfaf
Alex <amarcoli@ucsc.edu>
2014-07-17 00:22:49 -0700

    Added slime sprites and logic for cool effect

--- Assets/Scripts/Slime/Slime.cs
+++ Assets/Scripts/Slime/Slime.cs
@@ -5,10 +5,14 @@ using System.Collections.Generic;
 public class Slime : MonoBehaviour {
     public const float OPACITY_CHANGE_SPEED = 1.0f;
     public const float HEALTH_REGEN_RATE = 0.1f;
-    public const float TIME_PER_EXPAND = 0.1f;
+    public const float TIME_PER_EXPAND = 1.0f;
+
+    private static Sprite[] _slimeSpriteLookup = null;
+    private static int[] _slimeSpriteAngleLookup = { 0, 0, 90, 0, 180, 0, 90, 0, 270, 270, 180, 270, 180,  180, 90, 0 };
 
     private Tile _myTile;
     private Tilemap _tilemap;
+    private SpriteRenderer _slimeRenderer;
 
     private bool _isSolid = false;
     private float _percentHealth = 1.0f;
@@ -19,19 +23,55 @@ public class Slime : MonoBehaviour {
     private Path _currentExpandPath = null;
     private float _timeUntilExpand = 0.0f;
 
+    private void initSlimeSprites() {
+        Sprite slimeSprite0x1 = Resources.Load<Sprite>("Sprites/Slime/Slime-0x1");
+        Sprite slimeSprite0x3 = Resources.Load<Sprite>("Sprites/Slime/Slime-0x3");
+        Sprite slimeSprite0x5 = Resources.Load<Sprite>("Sprites/Slime/Slime-0x5");
+        Sprite slimeSprite0x7 = Resources.Load<Sprite>("Sprites/Slime/Slime-0x7");
+        Sprite slimeSprite0xF = Resources.Load<Sprite>("Sprites/Slime/Slime-0xF");
+        _slimeSpriteLookup = new Sprite[16];
+        _slimeSpriteLookup[0x1] = slimeSprite0x1;
+        _slimeSpriteLookup[0x2] = slimeSprite0x1;
+        _slimeSpriteLookup[0x3] = slimeSprite0x3;
+        _slimeSpriteLookup[0x4] = slimeSprite0x1;
+        _slimeSpriteLookup[0x5] = slimeSprite0x5;
+        _slimeSpriteLookup[0x6] = slimeSprite0x3;
+        _slimeSpriteLookup[0x7] = slimeSprite0x7;
+        _slimeSpriteLookup[0x8] = slimeSprite0x1;
+        _slimeSpriteLookup[0x9] = slimeSprite0x3;
+        _slimeSpriteLookup[0xA] = slimeSprite0x5;
+        _slimeSpriteLookup[0xB] = slimeSprite0x7;
+        _slimeSpriteLookup[0xC] = slimeSprite0x3;
+        _slimeSpriteLookup[0xD] = slimeSprite0x7;
+        _slimeSpriteLookup[0xE] = slimeSprite0x7;
+        _slimeSpriteLookup[0xF] = slimeSprite0xF;
+    }
+
     public void Awake() {
+        if (_slimeSpriteLookup == null) {
+            initSlimeSprites();
+        }
+
         _tilemap = Tilemap.getInstance();
         _myTile = GetComponent<Tile>();
-        updateNeighbors(0);
+
+        GameObject slimeRendererObject = new GameObject("Slime");
+        slimeRendererObject.transform.parent = transform;
+        slimeRendererObject.transform.position = transform.position;
+        _slimeRenderer = slimeRendererObject.AddComponent<SpriteRenderer>();
+        _slimeRenderer.sortingLayerName = "Slime";
+
+        updateNeighborCount(true, 1);
     }
 
     public void OnDestroy() {
-        updateNeighbors(_isSolid ? -1 : 0);
+        updateNeighborCount(true, _isSolid ? -1 : 0);
     }
 
     public void forceSlimeUpdate(int modifyNeighborCount = 0) {
         enabled = true;
         _slimeNeighbors += modifyNeighborCount;
+        updateNeighborCount(false);
     }
 
     [ContextMenu ("Toggle Solidity")]
@@ -43,7 +83,6 @@ public class Slime : MonoBehaviour {
         if (_isSolid != isSolid) {
             _isSolid = isSolid;
             forceSlimeUpdate();
-            updateNeighbors(_isSolid ? 1 : -1);
 
             if (_isSolid) {
                 List<Tile> neighbors = _tilemap.getNeighboringTiles(transform.position);
@@ -53,6 +92,8 @@ public class Slime : MonoBehaviour {
                     }
                 }
             }
+
+            updateNeighborCount(true, _isSolid ? 1 : -1);
         }
     }
 
@@ -142,22 +183,36 @@ public class Slime : MonoBehaviour {
         return opacity;
     }
 
-    private void updateNeighbors(int modifyNeighborCount) {
+    private void updateNeighborCount(bool forceNeighborUpdates = false, int modifyNeighborCount = 0) {
         List<Tile> neighbors = _tilemap.getNeighboringTiles(transform.position);
         _slimeNeighbors = 0;
+
+        int spriteMask = 0;
+
         foreach (Tile tile in neighbors) {
             Slime slime = tile.GetComponent<Slime>();
             if (slime != null) {
                 if (slime._isSolid) {
+                    Vector2Int delta = Tilemap.getTilemapLocation(tile.transform.position) - Tilemap.getTilemapLocation(transform.position);
+                    int neighborMask = 0xF;
+                    if (delta.y == 1) neighborMask &= 0x3; //0011
+                    if (delta.y == -1) neighborMask &= 0xC; //1100
+                    if (delta.x == 1) neighborMask &= 0x6; //0110
+                    if (delta.x == -1) neighborMask &= 0x9; //1001
+                    spriteMask |= neighborMask;
+
                     _slimeNeighbors++;
                 }
-                slime.forceSlimeUpdate(modifyNeighborCount);
+                if (forceNeighborUpdates) {
+                    slime.forceSlimeUpdate(modifyNeighborCount);
+                }
             }
         }
-    }
 
-    public void OnDrawGizmos() {
-        Gizmos.color = new Color(1.0f, 0.0f, 1.0f, currentOpacity);
-        Gizmos.DrawCube(transform.position, Vector3.one);
+        if (_isSolid) {
+            spriteMask = 0xF;
+        }
+        _slimeRenderer.sprite = _slimeSpriteLookup[spriteMask];
+        _slimeRenderer.gameObject.transform.eulerAngles = new Vector3(0, 0, -_slimeSpriteAngleLookup[spriteMask]);
     }
 }

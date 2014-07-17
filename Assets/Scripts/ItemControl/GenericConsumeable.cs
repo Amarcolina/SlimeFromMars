57468ad7f63855c9869ad4073c9a5280a4751230
neonbluecow <sjcastag@ucsc.edu>
2014-07-16 21:57:41 -0700

    Item class started

---
+++ Assets/Scripts/ItemControl/GenericConsumeable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ElementalAttribute {
+    ACID,
+    BIO,
+    ELECTRCITY
+}
+
+public enum ItemSize {
+    SMALL,
+    MEDIUM,
+    LARGE
+}
+public class GenericConsumeable : MonoBehaviour {
+    
+
+    public ItemSize size; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

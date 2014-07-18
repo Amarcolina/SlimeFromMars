4393bd6adcc78d87bb351d6f52a3a3affd981da9
Alex <amarcoli@ucsc.edu>
2014-07-17 20:40:10 -0700

    Grabbed the NGUI fixes from Galaga

--- Assets/NGUI/Scripts/Editor/NGUITransformInspector.cs
+++ Assets/NGUI/Scripts/Editor/NGUITransformInspector.cs
@@ -178,7 +178,7 @@ public class NGUITransformInspector : Editor
 
 	public override void OnInspectorGUI ()
 	{
-		EditorGUIUtility.LookLikeControls(15f);
+        EditorGUIUtility.labelWidth = 15f;
 
 		serializedObject.Update();
 

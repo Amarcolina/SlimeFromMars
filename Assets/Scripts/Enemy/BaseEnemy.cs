4d41aeb5e9c847d8a61cb39b50b234aa9098d330
Ryan Ho <rho1@ucsc.edu>
2014-07-17 22:37:54 -0700

    begin work on enemies

---
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	private float health;
+	private float speed;
+	private float damage;
+	public float visionRange;
+	private Vector2Int position;
+	public bool wander;
+	public bool seek;
+	public GameObject player;
+
+	public void Spawn(Vector2Int spawnPosition){
+		position = spawnPosition;
+	}
+
+	public void Wander(){
+		}
+
+	public void Chase(){
+	}
+
+	public void Update(){
+		if (health <= 0) {
+			GameObject.Destroy(this);		
+		}
+	}
+
+}

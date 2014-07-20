7bedf1789f182e8090e1b59b46cae4ca6a472d8e
Alex <amarcoli@ucsc.edu>
2014-07-19 21:49:13 -0700

    Added docs to IDamageable

--- Assets/Scripts/Enemy/IDamageable.cs
+++ Assets/Scripts/Enemy/IDamageable.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/* The generic IDamageable interface.  Any component that wants to be
+ * able to recieve damage should implement this interface.
+ */
 public interface IDamageable {
     void damage(float damage);
 }

5df1feae802434a82fb978481042ad0cbe8cfe17
Alex <amarcoli@ucsc.edu>
2014-07-22 16:41:40 -0700

    Moving slime renderer to new component

--- Assets/Scripts/Slime/Slime.cs
+++ Assets/Scripts/Slime/Slime.cs
@@ -6,48 +6,13 @@ public class Slime : MonoBehaviour {
     public const float OPACITY_CHANGE_SPEED = 1.0f;
     public const float HEALTH_REGEN_RATE = 0.1f;
     public const float TIME_PER_EXPAND = 0.05f;
-    public bool startSolid = false;
 
-    private static Sprite[] _slimeSpriteLookup = null;
-    private static int[] _slimeSpriteAngleLookup = { 0, 0, 90, 0, 180, 0, 90, 0, 270, 270, 180, 270, 180,  180, 90, 0 };
-
-    private bool _isSolid = false;
     private Tilemap _tilemap;
-    private SpriteRenderer _slimeRenderer;
     
     private float _percentHealth = 1.0f;
-    private int _solidSlimeNeighborCount = 0;
 
     private Path _currentExpandPath = null;
     private float _timeUntilExpand = 0.0f;
-
-    /* This initializes the _slimeSpriteLookup table.  This is a static table and so this
-     * method only needs to be called once.  The table is static so tat all slime objects
-     * can use the same sprites.
-     */
-    private void initSlimeSprites() {
-        Sprite slimeSprite0x1 = Resources.Load<Sprite>("Sprites/Slime/Slime-0x1");
-        Sprite slimeSprite0x3 = Resources.Load<Sprite>("Sprites/Slime/Slime-0x3");
-        Sprite slimeSprite0x5 = Resources.Load<Sprite>("Sprites/Slime/Slime-0x5");
-        Sprite slimeSprite0x7 = Resources.Load<Sprite>("Sprites/Slime/Slime-0x7");
-        Sprite slimeSprite0xF = Resources.Load<Sprite>("Sprites/Slime/Slime-0xF");
-        _slimeSpriteLookup = new Sprite[16];
-        _slimeSpriteLookup[0x1] = slimeSprite0x1;
-        _slimeSpriteLookup[0x2] = slimeSprite0x1;
-        _slimeSpriteLookup[0x3] = slimeSprite0x3;
-        _slimeSpriteLookup[0x4] = slimeSprite0x1;
-        _slimeSpriteLookup[0x5] = slimeSprite0x5;
-        _slimeSpriteLookup[0x6] = slimeSprite0x3;
-        _slimeSpriteLookup[0x7] = slimeSprite0x7;
-        _slimeSpriteLookup[0x8] = slimeSprite0x1;
-        _slimeSpriteLookup[0x9] = slimeSprite0x3;
-        _slimeSpriteLookup[0xA] = slimeSprite0x5;
-        _slimeSpriteLookup[0xB] = slimeSprite0x7;
-        _slimeSpriteLookup[0xC] = slimeSprite0x3;
-        _slimeSpriteLookup[0xD] = slimeSprite0x7;
-        _slimeSpriteLookup[0xE] = slimeSprite0x7;
-        _slimeSpriteLookup[0xF] = slimeSprite0xF;
-    }
 
     /* This method does the following:
      *      Initializes the slime sprite lookup table if it is not already initialized
@@ -56,27 +21,11 @@ public class Slime : MonoBehaviour {
      *      Lets all neighboring slimes know that this slime has been added
      */
     public void Awake() {
-        if (_slimeSpriteLookup == null) {
-            initSlimeSprites();
-        }
-
         _tilemap = Tilemap.getInstance();
 
         GameObject slimeRendererObject = new GameObject("Slime");
         slimeRendererObject.transform.parent = transform;
         slimeRendererObject.transform.position = transform.position;
-        _slimeRenderer = slimeRendererObject.AddComponent<SpriteRenderer>();
-        _slimeRenderer.sortingLayerName = "Slime";
-
-        if (startSolid) {
-            setSolid(true);
-        }
-
-        updateNeighborCount(true);
-    }
-
-    public void OnDestroy() {
-        Destroy(_slimeRenderer.gameObject);
     }
 
     /* Forces this slime to wake up.  This causes it to recount it's
@@ -85,36 +34,6 @@ public class Slime : MonoBehaviour {
      */
     public void wakeUpSlime() {
         enabled = true;
-        updateNeighborCount(false);
-    }
-
-    /* Sets whether or not this slime should be solid.  Solid slime blocks
-     * always stay around, even if they have no neighbors.  Un-Solid slime
-     * blocks can only exist if they are touching at least 1 solid slime block
-     * 
-     * This method handles the addition of new slime blocks that may need
-     * to be created, and also wakes the slime up
-     */
-    public void setSolid(bool shouldBeSolid) {
-        if (_isSolid != shouldBeSolid) {
-            _isSolid = shouldBeSolid;
-            wakeUpSlime();
-
-            if (_isSolid) {
-                List<Tile> neighbors = _tilemap.getNeighboringTiles(transform.position);
-                foreach (Tile tile in neighbors) {
-                    if (tile.GetComponent<Slime>() == null) {
-                        tile.gameObject.AddComponent<Slime>();
-                    }
-                }
-            }
-
-            updateNeighborCount(true);
-        }
-    }
-
-    public bool isSolid() {
-        return _isSolid;
     }
 
     /* The update loop is only proccessed if this slime is awake.
@@ -156,9 +75,6 @@ public class Slime : MonoBehaviour {
      */
     public void damageSlime(float percentDamage) {
         _percentHealth -= percentDamage;
-        if (_percentHealth <= 0.0f) {
-            setSolid(false);
-        }
         wakeUpSlime();
     }
 
@@ -185,7 +101,7 @@ public class Slime : MonoBehaviour {
         for (int i = 0; i < path.Count - 1; i++) {
             Vector2Int node = path[i];
             Slime slime = tilemap.getTile(node).GetComponent<Slime>();
-            if (slime == null || !slime.isSolid()) {
+            if (slime == null) {
                 cost++;
             }
         }
@@ -193,10 +109,6 @@ public class Slime : MonoBehaviour {
     }
 
     private void requestExpansionInternal(Path path, float residualTimeLeft) {
-        if (_isSolid) {
-            residualTimeLeft = 0.0f;
-        }
-        setSolid(true);
         wakeUpSlime();
 
         _currentExpandPath = path;
@@ -215,64 +127,25 @@ public class Slime : MonoBehaviour {
      */
     private void expandSlime() {
         Vector2Int nextNode = _currentExpandPath.getNext();
+        float residualTimeLeft = _timeUntilExpand + TIME_PER_EXPAND;
 
         Tile newSlimeTile = _tilemap.getTile(nextNode);
         if (newSlimeTile && newSlimeTile.isWalkable) {
             Slime newSlime = newSlimeTile.GetComponent<Slime>();
+            
             if(newSlime == null){
                 newSlime = newSlimeTile.gameObject.AddComponent<Slime>();
+            } else {
+                residualTimeLeft = 0.0f;
             }
 
             SlimeController.getInstance().setSelectedSlime(newSlime);
 
             if (_currentExpandPath.getNodesLeft() > 0) {
-                newSlime.requestExpansionInternal(_currentExpandPath, _timeUntilExpand + TIME_PER_EXPAND);
+                newSlime.requestExpansionInternal(_currentExpandPath, residualTimeLeft);
             }
         }
 
         _currentExpandPath = null;
-    }
-
-    /* Updates the solid neghbor count of this slime.  This also updates the sprite
-     * used to represent the slime based on the locations of neighboring solid
-     * slimes.
-     * 
-     * An optional bool allows this method to wake up neighboring slimes
-     */
-    private void updateNeighborCount(bool shouldWakeUpNeighbors = false) {
-        List<Tile> neighbors = _tilemap.getNeighboringTiles(transform.position);
-        _solidSlimeNeighborCount = 0;
-
-        int spriteMask = 0;
-
-        foreach (Tile tile in neighbors) {
-            Slime slime = tile.GetComponent<Slime>();
-            if (slime != null) {
-                if (slime._isSolid) {
-                    Vector2Int delta = Tilemap.getTilemapLocation(tile.transform.position) - Tilemap.getTilemapLocation(transform.position);
-                    int neighborMask = 0xF;
-                    if (delta.y == 1) neighborMask &= 0x3; //0011
-                    if (delta.y == -1) neighborMask &= 0xC; //1100
-                    if (delta.x == 1) neighborMask &= 0x6; //0110
-                    if (delta.x == -1) neighborMask &= 0x9; //1001
-                    spriteMask |= neighborMask;
-
-                    _solidSlimeNeighborCount++;
-                }
-                if (shouldWakeUpNeighbors) {
-                    slime.wakeUpSlime();
-                }
-            }
-        }
-
-        if (_isSolid) {
-            spriteMask = 0xF;
-        } else {
-            if (_solidSlimeNeighborCount == 0) {
-                Destroy(this);
-            }
-        }
-        _slimeRenderer.sprite = _slimeSpriteLookup[spriteMask];
-        _slimeRenderer.gameObject.transform.eulerAngles = new Vector3(0, 0, -_slimeSpriteAngleLookup[spriteMask]);
     }
 }
---
+++ Assets/Scripts/Slime/SlimeRenderer.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate bool IsCellSolidFunction(Vector2Int cellPosition);
+
+public class SlimeRenderer : MonoBehaviour {
+    private static Vector2Int[] _cellOffset = {new Vector2Int(-1, -1),
+                                               new Vector2Int( 0, -1),
+                                               new Vector2Int( 1, -1),
+                                               new Vector2Int( 1,  0),
+                                               new Vector2Int( 1,  1),
+                                               new Vector2Int( 0,  1),
+                                               new Vector2Int(-1,  1),
+                                               new Vector2Int(-1,  0),
+                                               new Vector2Int( 0,  0)};
+
+    private float[] _cellSolidity = new float[9];
+    private Texture2D _distanceRamp = null;
+    private IsCellSolidFunction _solidityFunction = null;
+    private Texture2D _texture = null;
+    private float _morphSpeed = 0.1f;
+    private Vector2Int _rendererPosition;
+    private SpriteRenderer _spriteRenderer;
+    private Tilemap _tilemap;
+
+    public void Awake() {
+        _texture = new Texture2D(Tile.TILE_PIXEL_SIZE, Tile.TILE_PIXEL_SIZE, TextureFormat.ARGB32, false);
+        _rendererPosition = Tilemap.getTilemapLocation(transform.position);
+        _tilemap = Tilemap.getInstance();
+
+        GameObject rendererGameObject = new GameObject("SlimeRenderer");
+        rendererGameObject.transform.parent = transform;
+        rendererGameObject.transform.position = transform.position;
+        _spriteRenderer = rendererGameObject.AddComponent<SpriteRenderer>();
+
+        _spriteRenderer.sprite = Sprite.Create(_texture, new Rect(0, 0, _texture.width, _texture.height), new Vector2(0.5f, 0.5f));
+    }
+
+    public void OnDestroy() {
+        Destroy(_spriteRenderer.gameObject);
+    }
+
+    public void setSolidityFunction(IsCellSolidFunction solidityFunction) {
+        _solidityFunction = solidityFunction;
+    }
+
+    public void setDistanceRamp(Texture2D distanceRamp) {
+        _distanceRamp = distanceRamp;
+    }
+
+    public void setMorphSpeed(float morphSpeed) {
+        _morphSpeed = morphSpeed;
+    }
+
+    public void wakeUpRenderer() {
+        enabled = true;
+    }
+
+    public void Update() {
+        bool canFallAsleep = true;
+        float soliditySum = 0.0f;
+
+        for (int i = 0; i < _cellSolidity.Length; i++) {
+            Vector2Int cellPosition = _cellOffset[i] + _rendererPosition;
+            float isSolid = _solidityFunction(cellPosition) ? 1.0f : 0.0f;
+            float newSolidity = Mathf.MoveTowards(_cellSolidity[i], isSolid, _morphSpeed * Time.deltaTime);
+            if (newSolidity != _cellSolidity[i]) {
+                _cellSolidity[i] = newSolidity;
+                soliditySum += newSolidity;
+                canFallAsleep = false;
+            }
+        }
+
+        updateNeighbors();
+        updateTexture();
+
+        if (canFallAsleep) {
+            enabled = false;
+            if (soliditySum == 0.0f) {
+                Destroy(this);
+            }
+        }
+    }
+
+    private void updateNeighbors() {
+        if(!_solidityFunction(_rendererPosition)){
+            return;
+        }
+        for (int i = 0; i < 8; i++) {
+            Vector2Int neighborPosition = _rendererPosition + _cellOffset[i];
+            GameObject tileObject = _tilemap.getTileGameObject(neighborPosition);
+            if (tileObject != null) {
+                SlimeRenderer[] renderers = tileObject.GetComponents<SlimeRenderer>();
+                bool hasSameRenderer = false;
+                foreach (SlimeRenderer otherRenderer in renderers) {
+                    if (isOfSameType(otherRenderer)) {
+                        hasSameRenderer = true;
+                        break;
+                    }
+                }
+
+                if (!hasSameRenderer) {
+                    SlimeRenderer newRenderer = tileObject.AddComponent<SlimeRenderer>();
+                    newRenderer.setDistanceRamp(_distanceRamp);
+                    newRenderer.setMorphSpeed(_morphSpeed);
+                    newRenderer.setSolidityFunction(_solidityFunction);
+                }
+            }
+        }
+    }
+
+    private bool isOfSameType(SlimeRenderer slimeRenderer) {
+        return slimeRenderer._solidityFunction == _solidityFunction &&
+               slimeRenderer._distanceRamp == _distanceRamp;
+    }
+
+    private void updateTexture() {
+        for (int x = 0; x < _texture.width; x++) {
+            for (int y = 0; y < _texture.height; y++) {
+                //float pointX = (x - _texture.width / 2.0f) / _texture.width;
+                //float pointY = (y - _texture.height / 2.0f) / _texture.height;
+                //Vector2 point = new Vector2(x, y);
+                //Color c = getPointColor(point);
+                Color c = Color.green;
+                _texture.SetPixel(x, y, c);
+            }
+        }
+    }
+
+    private Color getPointColor(Vector2 point) {
+        float pointValue = calculatePointValue(point);
+        int rampValue = (int)(pointValue * _distanceRamp.height);
+        return _distanceRamp.GetPixel(0, rampValue);
+    }
+
+    private float calculatePointValue(Vector2 point) {
+        float pointValue = 1.0f;
+        for (int i = 0; i < _cellOffset.Length; i++) {
+            Vector2Int cellPosInt = _rendererPosition + _cellOffset[i];
+            Vector2 cellPos = new Vector2(cellPosInt.x, cellPosInt.y);
+            pointValue = Mathf.Min(pointValue, distanceField(Vector2.zero, cellPos, point));
+        }
+
+        for (int i = 1; i < _cellOffset.Length; i += 2) {
+            Vector2Int cellPosInt0 = _rendererPosition + _cellOffset[i];
+            Vector2 cellPos0 = new Vector2(cellPosInt0.x, cellPosInt0.y);
+
+            Vector2Int cellPosInt1 = _rendererPosition + _cellOffset[(i+2)%_cellOffset.Length];
+            Vector2 cellPos1 = new Vector2(cellPosInt1.x, cellPosInt1.y);
+
+            pointValue = Mathf.Min(pointValue, distanceField(cellPos0, cellPos1, point));
+        }
+
+        return pointValue;
+    }
+
+    private float distanceField(Vector2 seg0, Vector2 seg1, Vector2 point) {
+        float t = Mathf.Clamp01(Vector2.Dot(point - seg0, seg1 - seg0) / Vector2.SqrMagnitude(seg0 - seg1));
+        return ((seg1 - seg0) * t + seg0 - point).magnitude;
+    }
+}

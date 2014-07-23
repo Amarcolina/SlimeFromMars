1685f2b5d61d8a65de025c864eb2e8d6040bf4e0
Alex <amarcoli@ucsc.edu>
2014-07-23 02:49:11 -0700

    Added docs to EnemyBase

--- Assets/Scripts/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -20,6 +20,15 @@ public class BaseEnemy : MonoBehaviour{
         return tileGameObject.GetComponent<Slime>() != null;
     }
 
+    /* Calling this function every frame will result in the enemy following
+     * their set movement path.  This handles everything from pathing using
+     * Astar to automatically re-pathing if a path gets blocked.
+     * 
+     * If you are resuming a movement pattern after moving off course,
+     * it is recomended to call recalculateMovementPatternPath() so that
+     * the path is recalculated to be from your current position, rather than
+     * trying to use the old stale path
+     */
     protected Waypoint followMovementPattern(float speed = 2.5f) {
         Waypoint currentWaypoint = movementPattern[_waypointIndex];
 
@@ -47,6 +56,11 @@ public class BaseEnemy : MonoBehaviour{
         return currentWaypoint;
     }
 
+    /* Recalculates the current path being used to follow the 
+     * enemies movement pattern.  Use this if you want to update the
+     * path (if the path has become stale or if you want to ensure that
+     * the shortest path is followed after the environmnt has changed)
+     */
     protected void recalculateMovementPatternPath(Waypoint waypoint = null) {
         if (waypoint == null) {
             waypoint = movementPattern[_waypointIndex];
@@ -56,6 +70,10 @@ public class BaseEnemy : MonoBehaviour{
                                               Tilemap.getTilemapLocation(waypoint.transform.position));
     }
 
+    /* Calling this method every frame will move the enemy towards a given destination
+     * at a specific speed.  This method will return true once the enemy has reached
+     * that destination
+     */
     protected bool moveTowardsPoint(Vector2 destination, float speed = 2.5f) {
         transform.position = Vector2.MoveTowards(transform.position, destination, speed * Time.deltaTime);
         return new Vector2(transform.position.x, transform.position.y) == destination;
@@ -65,6 +83,10 @@ public class BaseEnemy : MonoBehaviour{
         return moveTowardsPoint(Tilemap.getWorldLocation(target), speed);
     }
 
+    /* Calling this method every frame will move the enemy allong a given path
+     * at a specific speed.  This method will return true once the enemy has
+     * reached the end of the path.
+     */
     protected bool followPath(Path path, float speed = 2.5f) {
         Vector2Int node = path.getCurrent();
         if (moveTowardsPoint(node, speed)) {

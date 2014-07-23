2d0ecc9092b30e2e23df147d45228554a66953d3
robbsteel <rsteel@ucsc.edu>
2014-07-23 02:21:14 -0700

    Elemental UI, with script functions for each ability.

--- Assets/Scripts/Controllers/GameUI.cs
+++ Assets/Scripts/Controllers/GameUI.cs
@@ -12,6 +12,7 @@ public class GameUI : MonoBehaviour {
 	public GameObject LightningContainer_GameObject;
 	public GameObject BioContainer_GameObject;
 	public GameObject RadiationContainer_GameObject;
+	public GameObject PopoutMenu_GameObject;
 
 	private static GameUI _gameuiInstance;
 	public static GameUI getInstance() {
@@ -44,6 +45,44 @@ public class GameUI : MonoBehaviour {
 		RadiationContainer_GameObject.SetActive(true);
 		RadiationLevel_Label.text = "Radiation Level: " + CurrentLevel;
 	}
+
+	//Two functions called upon clicking either skills or close, which open and close the skills panel.
+	public void MenuActivated(){
+		PopoutMenu_GameObject.SetActive (true);
+	}
+
+	public void MenuDeactivated(){
+		PopoutMenu_GameObject.SetActive (false);
+	}
+
+
+	// A set of functions set out to be used upon clicking the abilities in the skills panel. Each one corresponds to the icon
+	// or ability on the panel.
+
+	public void RadiationOffense(){
+
+	}
+
+	public void RadiationDefense(){
+
+	}
+
+	public void LightningOffense(){
+
+	}
+
+	public void LightningDefense(){
+
+	}
+
+	public void BioOffense(){
+
+	}
+
+	public void BioDefense(){
+
+	}
+
 
 	// Use this for initialization
 	void Start () {

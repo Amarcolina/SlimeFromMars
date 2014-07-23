7d09ec21c53e9488b5193cdc77ee9da7dc931571
Alex <amarcoli@ucsc.edu>
2014-07-22 21:55:03 -0700

    Working on new waypoint system

---
+++ Assets/Scripts/Enemy/MovementPattern.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum PatternEndAction {
+    PING_PONG,
+    LOOP
+}
+
+[System.Serializable]
+public class MovementPatternInfo{
+    public MovementPattern pattern = null;
+    [MinValue (0)]
+    public int repeatCount = 0;
+}
+
+public class MovementPattern : MonoBehaviour {
+    public List<MovementPatternInfo> movementPatterns = new List<MovementPatternInfo>();
+    public PatternEndAction loopAction = PatternEndAction.LOOP;
+
+    private int _patternIndex = 0;
+
+    public virtual MovementWaypoint getNextWaypoint() {
+        MovementWaypoint waypoint = movementPatterns[_patternIndex].;
+
+
+
+        return waypoint;
+    }
+}
---
+++ Assets/Scripts/Enemy/MovementWaypoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum WaypointAction {
+    WAIT,
+    EXAMINE,
+    ACTION
+}
+
+public class MovementWaypoint : MovementPattern {
+    public float duration = 1.0f;
+    public WaypointAction waypointAction;
+}

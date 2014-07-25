7d646cd00fe8a528181f0ce79e61a74f6e29efcc
Ryan Ho <rho1@ucsc.edu>
2014-07-24 21:38:15 -0700

    Adds a pause menu to the game

---
+++ Assets/Scripts/MainMenu/UIVolumeControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIVolumeControl : MonoBehaviour {
+
+        private UISlider _Volumeslider;
+        
+        // Use this for initialization
+        void Start ()
+        {
+
+        }
+
+        void Update(){
+                GameObject root = GameObject.FindGameObjectWithTag ("UI");
+                PauseMenu pauseMenu = root.GetComponent<PauseMenu> ();
+                if(!pauseMenu.muted){
+                        _Volumeslider = gameObject.GetComponent<UISlider>();
+                        _Volumeslider.sliderValue = AudioListener.volume;
+                        _Volumeslider.onValueChange += OnValueChange;
+                }
+        }
+        
+        void OnValueChange(float val)
+        {
+                GameObject root = GameObject.FindGameObjectWithTag ("UI");
+                PauseMenu pauseMenu = root.GetComponent<PauseMenu>();
+                if (!pauseMenu.muted)
+                {
+                AudioListener.volume = val;
+                }
+        }
+}

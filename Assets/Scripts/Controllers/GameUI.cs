1da8d4488d438fc5d5e3fc9b2ea1794a2737bb65
robbsteel <rsteel@ucsc.edu>
2014-07-25 22:06:35 -0700

    Non-Active UI with logic, Bools for active UI, UI tweaks

--- Assets/Scripts/Controllers/GameUI.cs
+++ Assets/Scripts/Controllers/GameUI.cs
@@ -13,6 +13,18 @@ public class GameUI : MonoBehaviour {
 	public GameObject BioContainer_GameObject;
 	public GameObject RadiationContainer_GameObject;
 	public GameObject PopoutMenu_GameObject;
+	public GameObject SkillsToggleButton_GameObject;
+	//GameObject References, for toggling shadows on non-unlocked abilities
+	public GameObject ElectricityActive_GameObject;
+	public GameObject BioActive_GameObject;
+	public GameObject RadiationActive_GameObject;
+	public GameObject ElectricityNonActive_GameObject;
+	public GameObject BioNonActive_GameObject;
+	public GameObject RadiationNonActive_GameObject;
+	//Private bools for only turning on the shadowed elemental UI once.
+	private bool ElectricityActivated = false;
+	private bool BioActivated = false;
+	private bool RadiationActivated = false;
 
 	private static GameUI _gameuiInstance;
 	public static GameUI getInstance() {
@@ -30,29 +42,46 @@ public class GameUI : MonoBehaviour {
 	//Updates the lightning level by taking the current level as a float. Will show it when first called.
 	//Takes in the current amount of resources. Pass the new final amount, not the amount being added.
 	public void LightningUpdate(float CurrentLevel){
-		LightningContainer_GameObject.SetActive(true);
+		if (!ElectricityActivated) {
+			LightningContainer_GameObject.SetActive (true);
+			ElectricityActive_GameObject.SetActive(true);
+			ElectricityNonActive_GameObject.SetActive(false);
+			ElectricityActivated = true;
+		}
 		LightningLevel_Label.text = "Lightning Level: " + CurrentLevel;
 	}
 
 	//Updates the bio level by taking the current level as a float. Will show it when first called.
     public void BioUpdate(float CurrentLevel){
-		BioContainer_GameObject.SetActive(true);
+		if (!BioActivated) {
+			BioContainer_GameObject.SetActive (true);
+			BioActive_GameObject.SetActive(true);
+			BioNonActive_GameObject.SetActive(false);
+			BioActivated = true;
+		}
 		BioLevel_Label.text = "Bio Level: " + CurrentLevel;
 	}
 
 	//Updates the radiation level by taking the current level as a float. Will show it when first called.
 	public void RadiationUpdate(float CurrentLevel){
-		RadiationContainer_GameObject.SetActive(true);
+		if (!RadiationActivated) {
+			RadiationContainer_GameObject.SetActive (true);
+			RadiationActive_GameObject.SetActive(true);
+			RadiationNonActive_GameObject.SetActive(false);
+			RadiationActivated = true;
+		}
 		RadiationLevel_Label.text = "Radiation Level: " + CurrentLevel;
 	}
 
 	//Two functions called upon clicking either skills or close, which open and close the skills panel.
 	public void MenuActivated(){
+		SkillsToggleButton_GameObject.SetActive(false);
 		PopoutMenu_GameObject.SetActive (true);
 	}
 
 	public void MenuDeactivated(){
 		PopoutMenu_GameObject.SetActive (false);
+		SkillsToggleButton_GameObject.SetActive (true);
 	}
 
 

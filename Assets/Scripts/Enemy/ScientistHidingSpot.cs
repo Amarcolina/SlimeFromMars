d91c679daa7a3f002aa5e6a3a7a2cb5d9cb05528
Alex <amarcoli@ucsc.edu>
2014-07-31 14:53:25 -0700

    Added gizmos

--- Assets/Scripts/Enemy/ScientistHidingSpot.cs
+++ Assets/Scripts/Enemy/ScientistHidingSpot.cs
@@ -21,4 +21,13 @@ public class ScientistHidingSpot : MonoBehaviour {
     public void release() {
         _claimed = false;
     }
+
+    public void OnDrawGizmos() {
+        Gizmos.color = new Color(1.0f, 0.0f, 0.0f);
+        if (enterLocation != null) {
+            Gizmos.color = new Color(1.0f, 0.5f, 0.0f);
+            Gizmos.DrawLine(transform.position, enterLocation.position);
+        }
+        Gizmos.DrawSphere(transform.position, 0.15f);
+    }
 }

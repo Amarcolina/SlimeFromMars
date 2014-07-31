12326373cd684f01fb809c3ea7bd3de23dab054c
Alex <amarcoli@ucsc.edu>
2014-07-31 15:22:22 -0700

    Revamped scientists state machine

--- Assets/Scripts/Enemy/ScientistEnemy.cs
+++ Assets/Scripts/Enemy/ScientistEnemy.cs
@@ -7,19 +7,34 @@ public enum ScientistState {
     HIDING
 }
 
+
 public class ScientistEnemy : BaseEnemy {
+    public const float CHECK_RADIUS_FOR_HIDING_SPOTS = 25.0f;
+    public const float MAX_DISTANCE_TO_HIDING_SPOT_SQRD = 8.0f * 8.0f;
+
     public ScientistState startState = ScientistState.WANDERING;
 
     public float wanderSpeed = 2.5f;
     public float fleeSpeed = 3.5f;
 
     private ScientistState _currentState;
+    private delegate void StateFunction();
+    private StateFunction _currentExitFunction = null;
+    private StateFunction _currentStateFunction = null;
 
-    private float _timeStartedFleeing = 0;
+    private static ScientistHidingSpot[] _hidingSpots = null;
+    private ScientistHidingSpot _currentHidingSpot = null;
+    private ProximitySearcher<ScientistHidingSpot> _hidingSpotSearcher = null;
+    private Path _pathToHidingSpot = null;
 
     public override void Awake() {
         base.Awake();
-        _currentState = startState;
+        if (_hidingSpots == null) {
+            _hidingSpots = FindObjectsOfType<ScientistHidingSpot>();
+        }
+        _hidingSpotSearcher = new ProximitySearcher<ScientistHidingSpot>(_hidingSpots, CHECK_RADIUS_FOR_HIDING_SPOTS);
+
+        changeState(startState);
     }
 
     void Update() {
@@ -27,15 +42,26 @@ public class ScientistEnemy : BaseEnemy {
             return;
         }
 
+        _currentStateFunction();
+    }
+
+    private void changeState(ScientistState newState) {
+        if (_currentExitFunction != null) {
+            _currentExitFunction();
+            _currentExitFunction = null;
+        }
+
+        _currentState = newState;
         switch (_currentState) {
             case ScientistState.WANDERING:
-                wanderState();
+                _currentStateFunction = wanderState;
                 break;
             case ScientistState.FLEEING:
-                fleeState();
+                _currentStateFunction = fleeState;
                 break;
             case ScientistState.HIDING:
-                hideState();
+                _currentStateFunction = hideState;
+                _currentExitFunction = exitHideState;
                 break;
             default:
                 Debug.LogWarning("Cannot handle state " + _currentState);
@@ -45,18 +71,18 @@ public class ScientistEnemy : BaseEnemy {
 
     private void enterWanderState() {
         recalculateMovementPatternPath();
-        _currentState = ScientistState.WANDERING;
+        changeState(ScientistState.WANDERING);
     }
 
     private void wanderState() {
         followMovementPattern(wanderSpeed);
-        tryEnterFleeState();
+        //tryEnterFleeState();
+        tryEnterHidingState();
     }
 
     private bool tryEnterFleeState(int searchDistance = 20) {
         if (getNearestVisibleSlime(searchDistance) != null) {
-            _currentState = ScientistState.FLEEING;
-            _timeStartedFleeing = Time.time;
+            changeState(ScientistState.FLEEING);
             return true;
         }
         return false;
@@ -79,18 +105,42 @@ public class ScientistEnemy : BaseEnemy {
             enterWanderState();
         }
 
-        bool didFindHidingSpot = runAwayFromSlime(fleeSpeed);
-
-        if (didFindHidingSpot && Time.time - _timeStartedFleeing > 45.0f) {
-            enterHideState();
-        }
+        runAwayFromSlime(fleeSpeed);
+        tryEnterHidingState();
     }
 
-    private void enterHideState() {
-        _currentState = ScientistState.HIDING;
+    private bool tryEnterHidingState() {
+        ScientistHidingSpot spot = _hidingSpotSearcher.searchForClosest(transform.position, 9, 1);
+        if (getNearestVisibleSlime() != null) {
+            return false;
+        }
+
+        if ((transform.position - spot.transform.position).sqrMagnitude < MAX_DISTANCE_TO_HIDING_SPOT_SQRD){
+            if(spot.tryToClaim()){
+                _currentHidingSpot = spot;
+                _pathToHidingSpot = Astar.findPath(transform.position, spot.enterLocation.position);
+                changeState(ScientistState.HIDING);
+                return true;
+            }        
+        }
+        return false;
     }
 
     private void hideState() {
-        tryEnterFleeState(1);
+        if (_pathToHidingSpot != null) {
+            if (followPath(_pathToHidingSpot, fleeSpeed)) {
+                _pathToHidingSpot = null;
+            }
+        } else {
+            if(moveTowardsPoint(_currentHidingSpot.transform.position, fleeSpeed)){
+                tryEnterFleeState(1);
+            }
+        }
+    }
+
+    private void exitHideState() {
+        if (_currentHidingSpot != null) {
+            _currentHidingSpot.release();
+        }
     }
 }

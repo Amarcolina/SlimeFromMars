bab2affa84ccbebdabbe3af65807942e5fd5620e
Alex <amarcoli@ucsc.edu>
2014-07-31 14:39:43 -0700

    Added scientist hiding spot script

---
+++ Assets/Scripts/Enemy/ScientistHidingSpot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScientistHidingSpot : MonoBehaviour {
+    public Transform enterLocation = null;
+
+    private bool _claimed = false;
+
+    public bool tryToClaim() {
+        if (_claimed) {
+            return false;
+        }
+        _claimed = true;
+        return true;
+    }
+
+    public bool isClaimed() {
+        return _claimed;
+    }
+
+    public void release() {
+        _claimed = false;
+    }
+}

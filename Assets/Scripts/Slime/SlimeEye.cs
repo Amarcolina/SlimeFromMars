09c1d62a3432999d5b424ebf86e8a806a6b278a1
neonbluecow <sjcastag@ucsc.edu>
2014-08-06 16:25:19 -0700

    adding sprites
    adding sprites

---
+++ Assets/Scripts/Slime/SlimeEye.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimeEye : MonoBehaviour {
+    // Use this for initialization
+    SlimeController slime;
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        /*
+        if (slime.getSelectedSlime() == null) {
+            renderer.enabled = false;
+        }
+         */
+        movePupil();
+    }
+
+    public void movePupil() {
+       // if(transform.localPosition){
+         //   transform.localPosition = new Vector2(0, 0);
+        //} 
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.localPosition = mousePos;
+    }
+}

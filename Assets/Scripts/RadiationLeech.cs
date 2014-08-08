86c7b38a1873c4a8b0dcedf66a0e40181221ee5d
Alex <amarcoli@ucsc.edu>
2014-08-07 18:25:33 -0700

    Added RadiationLeech effect

---
+++ Assets/Scripts/RadiationLeech.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadiationLeech : MonoBehaviour {
+    private float _damagePeriod = 1.0f;
+    private float _damagePerPeriod = 0.1f;
+    private IDamageable _damageable = null;
+
+    private float _timeUntilNextDamage = 0.0f;
+    
+    public void Start() {
+        _damageable = GetComponent(typeof(IDamageable)) as IDamageable;
+    }
+
+    public void Update() {
+        _timeUntilNextDamage -= Time.deltaTime;
+        if (_timeUntilNextDamage <= 0.0f) {
+            _timeUntilNextDamage += _damagePeriod;
+            _damageable.damage(_damagePerPeriod);
+        }
+    }
+
+    public void setDamage(float damage) {
+        _damagePerPeriod = damage;
+    }
+
+    public void setDamagePeriod(float period) {
+        _damagePeriod = period;
+    }
+}

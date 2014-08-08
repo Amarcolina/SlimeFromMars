4be2c8d8cdaf89c0fcafa69495cf0cf7aa3f1739
Alex <amarcoli@ucsc.edu>
2014-08-08 15:51:54 -0700

    Removed bug

--- Assets/Scripts/Testing/AstarEnemy.cs
+++ Assets/Scripts/Testing/AstarEnemy.cs
@@ -7,11 +7,4 @@ public class AstarEnemy : BaseEnemy {
     public void Update() {
         moveTowardsPointAstar(goal.transform.position);
     }
-
-    public void OnDrawGizmos() {
-        Gizmos.color = new Color(0.0f, 1.0f, 0.0f, 0.3f);
-        foreach (Vector2Int pos in Astar.checkedNodesList) {
-            Gizmos.DrawCube(pos, Vector3.one);
-        }
-    }
 }

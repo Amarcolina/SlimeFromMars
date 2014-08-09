043a21583b31e26ac38dadacde95ef57e645f3a7
neonbluecow <sjcastag@ucsc.edu>
2014-08-09 15:55:19 -0700

    adding door controller class

---
+++ Assets/Scripts/Controllers/DoorController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+    private bool doorOpen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown("O")){
+            openDoor();
+        }
+        if (Input.GetKeyDown("C")) {
+            closeDoor();
+        }
+	}
+
+    public bool isDoorOpen() {
+        return doorOpen;
+    }
+
+    public void openDoor(){
+        //play opening animation and stop on open frame
+        
+    }
+
+    public void closeDoor(){
+        //play closing animation and stop on closed frame
+
+    }
+}

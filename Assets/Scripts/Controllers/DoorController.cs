05af0bf0b41a87ea8f43df5556b356fa4740335c
neonbluecow <sjcastag@ucsc.edu>
2014-08-09 21:43:57 -0700

    work on door cont.

--- Assets/Scripts/Controllers/DoorController.cs
+++ Assets/Scripts/Controllers/DoorController.cs
@@ -3,54 +3,57 @@ using System.Collections;
 
 public class DoorController : MonoBehaviour {
 
-    private bool doorOpen;
-    private Tile[] tilesUnderDoor;
-                                            
-
-	// Use this for initialization
-	void Start () {
-        doorOpen = false;
+    public bool doorOpen = false;
+    private Tile[] tilesUnderDoor = new Tile[6]; 
+    // Use this for initialization
+    void Start() {
         //Gets the offset of each tile under the door and stores them in an array for ease of use, later
-        tilesUnderDoor[0] = Tilemap.getInstance().getTile(transform.position + new Vector3( .5f, 0f, 0));
-        tilesUnderDoor[1] = Tilemap.getInstance().getTile(transform.position + new Vector3(-.5f, 0f, 0));	
-        tilesUnderDoor[2] = Tilemap.getInstance().getTile(transform.position + new Vector3( .5f, 1f, 0));	
-        tilesUnderDoor[3] = Tilemap.getInstance().getTile(transform.position + new Vector3(-.5f, 1f, 0));	
-        tilesUnderDoor[4] = Tilemap.getInstance().getTile(transform.position + new Vector3( .5f,-1f, 0));	
-        tilesUnderDoor[5] = Tilemap.getInstance().getTile(transform.position + new Vector3(-.5f,-1f, 0));	
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        if(Input.GetKeyDown(KeyCode.O)){
-            openDoor();
-        }
-        if (Input.GetKeyDown(KeyCode.C)) {
-            closeDoor();
-        }
-	}
+        tilesUnderDoor[0] = Tilemap.getInstance().getTile(transform.position + new Vector3(.5f, 0f, 0));
+        tilesUnderDoor[1] = Tilemap.getInstance().getTile(transform.position + new Vector3(-.5f, 0f, 0));
+        tilesUnderDoor[2] = Tilemap.getInstance().getTile(transform.position + new Vector3(.5f, 1f, 0));
+        tilesUnderDoor[3] = Tilemap.getInstance().getTile(transform.position + new Vector3(-.5f, 1f, 0));
+        tilesUnderDoor[4] = Tilemap.getInstance().getTile(transform.position + new Vector3(.5f, -1f, 0));
+        tilesUnderDoor[5] = Tilemap.getInstance().getTile(transform.position + new Vector3(-.5f, -1f, 0));
 
-    public bool isDoorOpen() {
-        return doorOpen;
+        if (!doorOpen) {
+            doorClosedState();
+        }
     }
 
-    public void openDoor(){
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.O) && !doorOpen) {
+            doorOpenedState();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.C) && doorOpen) {
+            doorClosedState();
+        }
+    }
+
+    public void doorOpenedState() {
+        doorOpen = true;
         //play opening animation and stop on open frame
         renderer.enabled = false;
-        
         //set tiles to walkable and slimeable
         for (int i = 0; i <= 5; i++) {
             tilesUnderDoor[i].isWalkable = true;
             tilesUnderDoor[i].isSlimeable = true;
+            tilesUnderDoor[i].isSpikeable = true;
+            tilesUnderDoor[i].isTransparent = true;
         }
-           
+
     }
 
-    public void closeDoor(){
+    public void doorClosedState() {
+        doorOpen = false;
         //play closing animation and stop on closed frame
         renderer.enabled = true;
         for (int i = 0; i <= 5; i++) {
             tilesUnderDoor[i].isWalkable = false;
             tilesUnderDoor[i].isSlimeable = false;
+            tilesUnderDoor[i].isSpikeable = false;
+            tilesUnderDoor[i].isTransparent = false;
         }
     }
 }

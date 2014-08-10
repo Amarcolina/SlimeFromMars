6670495fbdf8f5fe21b5f47c0f95eaa464f85ae9
neonbluecow <sjcastag@ucsc.edu>
2014-08-09 20:38:59 -0700

    continued work on door

--- Assets/Scripts/Controllers/DoorController.cs
+++ Assets/Scripts/Controllers/DoorController.cs
@@ -4,11 +4,20 @@ using System.Collections;
 public class DoorController : MonoBehaviour {
 
     private bool doorOpen;
+    private Tile[] tilesUnderDoor;
+                                            
 
 	// Use this for initialization
 	void Start () {
         doorOpen = false;
-	}
+        //Gets the offset of each tile under the door and stores them in an array for ease of use, later
+        tilesUnderDoor[0] = Tilemap.getInstance().getTile(transform.position + new Vector3( .5f, 0f, 0));
+        tilesUnderDoor[1] = Tilemap.getInstance().getTile(transform.position + new Vector3(-.5f, 0f, 0));	
+        tilesUnderDoor[2] = Tilemap.getInstance().getTile(transform.position + new Vector3( .5f, 1f, 0));	
+        tilesUnderDoor[3] = Tilemap.getInstance().getTile(transform.position + new Vector3(-.5f, 1f, 0));	
+        tilesUnderDoor[4] = Tilemap.getInstance().getTile(transform.position + new Vector3( .5f,-1f, 0));	
+        tilesUnderDoor[5] = Tilemap.getInstance().getTile(transform.position + new Vector3(-.5f,-1f, 0));	
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -27,12 +36,21 @@ public class DoorController : MonoBehaviour {
     public void openDoor(){
         //play opening animation and stop on open frame
         renderer.enabled = false;
-        //get tile offsets under door
-
+        
+        //set tiles to walkable and slimeable
+        for (int i = 0; i <= 5; i++) {
+            tilesUnderDoor[i].isWalkable = true;
+            tilesUnderDoor[i].isSlimeable = true;
+        }
+           
     }
 
     public void closeDoor(){
         //play closing animation and stop on closed frame
         renderer.enabled = true;
+        for (int i = 0; i <= 5; i++) {
+            tilesUnderDoor[i].isWalkable = false;
+            tilesUnderDoor[i].isSlimeable = false;
+        }
     }
 }

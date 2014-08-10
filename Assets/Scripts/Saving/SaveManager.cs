d038edbc7f7c260f8b7106b29f485604855b5449
Alex <amarcoli@ucsc.edu>
2014-08-09 21:32:06 -0700

    commenting and cleaning

--- Assets/Scripts/Saving/SaveManager.cs
+++ Assets/Scripts/Saving/SaveManager.cs
@@ -21,40 +21,45 @@ public class SaveManager : MonoBehaviour {
         }
     }
 
+    /* This is called when the level is loaded, and causes the game to be modified
+     * to suite the state of the current save.  
+     */
     public void OnLevelWasLoaded() {
-        SaveMarker[] markers = FindObjectsOfType<SaveMarker>();
-        foreach(SaveMarker marker in markers){
-            if(_currentSavedGame.destroyedObjects.Contains(marker.serialID)){
-                Destroy(marker.gameObject);
-                continue;
+        if (_isLoading) {
+            SaveMarker[] markers = FindObjectsOfType<SaveMarker>();
+            foreach (SaveMarker marker in markers) {
+                if (_currentSavedGame.destroyedObjects.Contains(marker.serialID)) {
+                    Destroy(marker.gameObject);
+                    continue;
+                }
+
+                SavedGameObjectData data;
+                if (_currentSavedGame.modifiedGameObjects.TryGetValue(marker.serialID, out data)) {
+                    marker.loadData(data);
+                    continue;
+                }
             }
 
-            SavedGameObjectData data;
-            if (_currentSavedGame.modifiedGameObjects.TryGetValue(marker.serialID, out data)) {
-                marker.loadData(data);
-                continue;
+            foreach (var newObjectPair in _currentSavedGame.newGameObjects) {
+                GameObject newObject;
+                SaveMarker newMarker;
+                if (newObjectPair.Value.prefabPath != null && newObjectPair.Value.prefabPath.Length != 0) {
+                    newObject = InstantiateSaved(newObjectPair.Value.prefabPath, Vector3.zero, Quaternion.identity);
+                    newMarker = newObject.GetComponent<SaveMarker>();
+                } else {
+                    newObject = new GameObject("Recreated Game Object");
+                    newMarker = newObject.AddComponent<SaveMarker>();
+                }
+
+                newMarker.serialID = newObjectPair.Key;
+                newMarker.loadData(newObjectPair.Value);
             }
+
+            _currentDestroyedObjects.Clear();
+            _currentDestroyedObjects.UnionWith(_currentSavedGame.destroyedObjects);
+
+            _isLoading = false;
         }
-
-        foreach (var newObjectPair in _currentSavedGame.newGameObjects) {
-            GameObject newObject;
-            SaveMarker newMarker;
-            if (newObjectPair.Value.prefabPath != null && newObjectPair.Value.prefabPath.Length != 0) {
-                newObject = InstantiateSaved(newObjectPair.Value.prefabPath, Vector3.zero, Quaternion.identity);
-                newMarker = newObject.GetComponent<SaveMarker>();
-            } else {
-                newObject = new GameObject("Recreated Game Object");
-                newMarker = newObject.AddComponent<SaveMarker>();
-            }
-                
-            newMarker.serialID = newObjectPair.Key;
-            newMarker.loadData(newObjectPair.Value);
-        }
-
-        _currentDestroyedObjects.Clear();
-        _currentDestroyedObjects.UnionWith(_currentSavedGame.destroyedObjects);
-
-        _isLoading = false;
     }
 
     public void Update() {
@@ -71,6 +76,10 @@ public class SaveManager : MonoBehaviour {
         }
     }
 
+    /* Use this if you want to Instantiate a prefab that you want to persist when
+     * a game is saved and reloaded.  Using this method stores the relevant information
+     * to re-create the prefab in the correct way when the game is re-loaded
+     */
     public static GameObject InstantiateSaved(string prefabPath, Vector3 position, Quaternion rotation) {
         GameObject prefab = Resources.Load<GameObject>(prefabPath);
         GameObject obj = Instantiate(prefab, position, rotation) as GameObject;
@@ -84,6 +93,10 @@ public class SaveManager : MonoBehaviour {
         return obj;
     }
 
+    /* This method is called by a SaveMarker whenever it is destroyed.  This keeps track
+     * of any methods that have been destroyed so that when a game is re-loaded, 
+     * any objects that should be destroyed can do so.
+     */
     private HashSet<int> _currentDestroyedObjects = new HashSet<int>();
     public void recordObjectDestruction(int serialID) {
         if (_isLoading) {
@@ -99,10 +112,14 @@ public class SaveManager : MonoBehaviour {
         }
     }
 
+    /* Calling this saves the current game
+     */
     public void saveGame() {
         SaveMarker[] existingMarkers = FindObjectsOfType<SaveMarker>();
         foreach (SaveMarker marker in existingMarkers) {
             SavedGameObjectData savedData = marker.saveData();
+
+            //If the saved data is null, there has been no change so we don't need to save it
             if (savedData == null) {
                 continue;
             }
@@ -119,6 +136,9 @@ public class SaveManager : MonoBehaviour {
         _currentSavedGame.destroyedObjects.UnionWith(_currentDestroyedObjects);
     }
 
+    /* Calling this loads the game.  This first causes a level reload, after which
+     * the game is modified to be equal to the save state
+     */
     public void loadGame() {
         _isLoading = true;
         Application.LoadLevel(Application.loadedLevel);

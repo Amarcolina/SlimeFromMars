03ac728e7c584003a17d50a322c4d34f2738c48d
neonbluecow <sjcastag@ucsc.edu>
2014-08-09 19:33:02 -0700

    adding door and sprite

--- Assets/Scripts/Controllers/DoorController.cs
+++ Assets/Scripts/Controllers/DoorController.cs
@@ -7,15 +7,15 @@ public class DoorController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        doorOpen = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyDown("O")){
+        if(Input.GetKeyDown(KeyCode.O)){
             openDoor();
         }
-        if (Input.GetKeyDown("C")) {
+        if (Input.GetKeyDown(KeyCode.C)) {
             closeDoor();
         }
 	}
@@ -26,11 +26,13 @@ public class DoorController : MonoBehaviour {
 
     public void openDoor(){
         //play opening animation and stop on open frame
-        
+        renderer.enabled = false;
+        //get tile offsets under door
+
     }
 
     public void closeDoor(){
         //play closing animation and stop on closed frame
-
+        renderer.enabled = true;
     }
 }
